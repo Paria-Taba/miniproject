07f75e4bc1c2fef16c107fa84880f8cd10f380ca
paria <pariyataba@gmail.com>
2025-10-09 13:31:29 +0200

    set up express in backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,9 @@
-console.log("paria")
+import express from "express"
+const app=express()
+import dotenv from "dotenv"
+dotenv.config()
+const port=process.env.PORT
+
+app.listen(port,()=>{
+	console.log(`server run on port ${port}`)
+})
